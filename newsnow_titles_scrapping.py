5628a95fdd161f5b524e2a9856729d071ef0696f
n0m1nd <n0m1nd@users.noreply.github.com>
2020-12-18 10:21:12 +0200

    Create newsnow_titles_scrapping.py
    The executed script returns the titles list of articles from the newsnow.co.uk website.
    The results are located in the titles_list.txt file in the script directory.

---
+++ newsnow_titles_scrapping.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 09:14:15 2020
+
+@author: nomind
+"""
+
+# Libraries import
+import requests
+from bs4 import BeautifulSoup
+
+# Setting the URL
+url = 'https://www.newsnow.co.uk/h/'
+
+# Connect to the URL and get response
+response = requests.get(url)
+
+# If server response is 200 then proceed
+if response.status_code == requests.codes.ok:
+    # Parse HTML and save to BeautifulSoup object
+    soup = BeautifulSoup(response.text, "html.parser")
+    # Get a list of titles and write to file
+    # Open file for writing
+    with open('titles_list.txt', 'w') as titles_list:   
+        # Get elements with 'a class="hll"' where titles are
+        for element in soup.find_all('a', class_='hll'):
+            # Write every title to file
+            titles_list.write(element.text + '\n') 
+    # Close file        
+    titles_list.close()
+# If server response is not OK - contact script developer
+else:
+    print ("Server response is not 200. Please contact John Smith for script fix")
